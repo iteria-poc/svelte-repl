4e91be68f4e234350e3a1adc59a4f959da556f5c
Jozef Slezak <jozef.slezak@gmail.com>
2021-04-09 05:17:52 +0200

    import svelte from 'rollup-plugin-svelte';

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
 import { terser } from 'rollup-plugin-terser';
 
-
+import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
